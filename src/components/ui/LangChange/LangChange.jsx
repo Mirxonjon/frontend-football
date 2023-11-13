33ffd0b5c206b68f4d588d00cbb5ca06984de07f
elshodbek <elshodtukhtamurodov13@gmail.com>
2023-11-13 22:51:00 +0500

    Revert "asdas"
    This reverts commit f4097bc51a7e6adeaa7feb8355bec130a5a97f71.

--- src/components/ui/LangChange/LangChange.jsx
+++ src/components/ui/LangChange/LangChange.jsx
@@ -4,36 +4,24 @@ import { useDispatch, useSelector } from "react-redux";
 
 import "./LangChange.scss";
 import { editLang } from "../../../store/slice/lang";
-import { useEffect } from "react";
 
 function LangChange() {
   const [isOpenLang, setIsOpenLang] = useState(false);
   const dispatch = useDispatch();
-  // eslint-disable-next-line no-unused-vars
-  const { lang, loading } = useSelector((state) => state.lang);
+  const { lang } = useSelector((state) => state.lang);
+
   const handleLang = () => {
     if (isOpenLang) {
-      document.querySelector(".header_lang_select").style.display = "none";
-    } else {
       document.querySelector(".header_lang_select").style.display = "block";
+    } else {
+      document.querySelector(".header_lang_select").style.display = "none";
     }
     setIsOpenLang(!isOpenLang);
   };
+
   const handleLanguageSelect = (selectedLang) => {
     dispatch(editLang(selectedLang));
   };
-  useEffect(() => {
-    document.body.addEventListener("click", (e) => {
-      if (
-        !e.target.closest(".header_lang") &&
-        document.querySelector(".header_lang_select").style.display === "block"
-      ) {
-        document.querySelector(".header_lang_select").style.display = "none";
-        setIsOpenLang(false);
-      }
-    });
-  }, []);
-
   return (
     <div className="header_lang" onClick={handleLang}>
       <GlobalOutlined /> {lang}
